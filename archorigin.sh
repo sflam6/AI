a4418eb7622b9a5f4602d3cd539bf9e5ca8b0aa3
sflam6 <162942476+sflam6@users.noreply.github.com>
2024-03-18 22:43:01 +0800

    Update archorigin.sh

--- archorigin.sh
+++ archorigin.sh
@@ -78,7 +78,7 @@ sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 
-ln -sf /usr/share/zoneinfo/Asia/Kathmandu /etc/localtime
+ln -sf /usr/share/zoneinfo/Asia/Hongkong /etc/localtime
 hwclock --systohc
 
 echo "arch" > /etc/hostname
