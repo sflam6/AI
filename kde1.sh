079a84cda919c9748b53638a64ff7714c99255d3
sflam6 <162942476+sflam6@users.noreply.github.com>
2024-03-19 16:43:08 +0800

    Update and rename InKde.sh to kde1.sh

--- kde1.sh
+++ kde1.sh
@@ -1,7 +1,7 @@
 # Display, firmware
 sudo pacman -Syy && -Syu
 sudo pacman -S mesa-vdpau linux-firmware packagekit-qt5 fwupd flatpak linux linux-headers --noconfirm --needed
-sudo pacman -S flameshot steam fuse2 unrar libreoffice-still  qt5-wayland qt6-wayland partitionmanager --noconfirm --needed
+sudo pacman -S flameshot steam fuse2 unrar libreoffice-still  qt5-wayland qt6-wayland partitionmanager power-profile-daemon --noconfirm --needed
 sudo pacman -S fcitx5-im fcitx5-qt fcitx5-gtk fcitx5-table-extra avahi --noconfirm --needed
 
 # Flatpak packages
